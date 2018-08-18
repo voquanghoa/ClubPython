601439c56973ae6a66d5db14a6d90df905a6b1af
Võ Quang Hòa <hvquang@openwt.com>
2018-08-18 15:52:40 +0700

    Rename class

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,13 +4,10 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import club.views
 
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    url(r'^$', club.views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
