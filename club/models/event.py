15fddc086469f1f510d804952448ed900e45abef
Võ Quang Hòa <hvquang@openwt.com>
2018-08-20 00:07:59 +0700

    Update

--- club/models/event.py
+++ club/models/event.py
@@ -1,21 +1,29 @@
+
+
 from django.db import models
 from django.contrib import admin
+from django.utils.datetime_safe import datetime
 from rest_framework import serializers
 
 
 class Event(models.Model):
+    title = models.CharField(max_length=256, default='')
+    description = models.CharField(max_length=2048)
+    date_time = models.DateTimeField(default=datetime.now, blank=True)
+    place = models.CharField(max_length=2048, default='')
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
     created_time = models.DateTimeField()
     update_time = models.DateTimeField()
-    description = models.CharField(max_length=2048)
 
 
 class EventForm(admin.ModelAdmin):
     class Meta:
         model = Event
-        fields = ['description', 'created_time', 'update_time']
+        fields = ['title', 'description', 'date_time', 'place', 'latitude', 'longitude']
 
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ["id", "description"]
+        fields = ['id', 'title', 'description', 'date_time', 'place', 'latitude', 'longitude']
